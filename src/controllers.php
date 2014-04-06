280bd3c14778cdbf789e677fdafc7c009fd2811f
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2014-04-06 02:43:15 -0500

    Openshift support

--- src/controllers.php
+++ src/controllers.php
@@ -7,9 +7,39 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array('page_title', '123'));
+    $query = $app["orm.em"]->createQuery("SELECT t FROM Tineo\Entity\Entry t ORDER BY t.datePublished DESC");
+    $entries = $query->getArrayResult();
+    return $app['twig']->render('index.twig', array('entries' => $entries ));
 })
 ->bind('homepage')
+;
+
+
+
+$app->get('/blog', function () use ($app) {
+    return $app['twig']->render('blog.twig', array('page_title' => '123'));
+})
+    ->bind('blog')
+;
+
+$app->get('/contact', function () use ($app) {
+    return $app['twig']->render('contact.twig', array('page_title' => '123'));
+})
+    ->bind('contact')
+;
+
+$app->get('/{slug}', function ($slug) use ($app) {
+    $query = $app["orm.em"]->createQuery("SELECT t FROM Tineo\Entity\Entry t WHERE t.slug = :slug");
+    $query->setParameter(':slug', $slug);
+    $entry = $query->getOneOrNullResult();
+
+    if(count($entry) == 0)
+        return new Response($app['twig']->render('404.twig', array('code' => 404)), 404);
+
+
+    return $app['twig']->render('post.twig', array('entry' => $entry));
+})
+    ->bind('post')
 ;
 
 $app->error(function (\Exception $e, $code) use ($app) {
@@ -17,7 +47,8 @@ $app->error(function (\Exception $e, $code) use ($app) {
         return;
     }
 
-    $page = 404 == $code ? '404.html' : '500.html';
+    $page = 404 == $code ? '404.twig' : '500.twig';
 
     return new Response($app['twig']->render($page, array('code' => $code)), $code);
+
 });
